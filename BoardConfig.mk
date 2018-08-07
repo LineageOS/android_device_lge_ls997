c48679e0fc36ce610fcd1610a61570d90ce93896
Eric Meddaugh <eric@meddaughs.com>
2018-08-07 18:19:54 -0400

    ls997: Add TARGET_SYSTEM_PARTITION for system-as-root layout
    Change-Id: If441f69f0d992afec679dc67e86350d939b68e72

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,7 @@ DEVICE_PATH := device/lge/ls997
 -include device/lge/v20-common/BoardConfigCommon.mk
 
 TARGET_OTA_ASSERT_DEVICE := ls997,us996,elsa
+TARGET_SYSTEM_PARTITION := /dev/sda18
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_ls997_defconfig
