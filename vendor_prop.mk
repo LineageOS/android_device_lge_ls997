965c30ec09ef43b2d50363be4f3dd6ca928202ca
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-16 10:19:18 -0700

    ls997: Move device specific props to vendor
    Change-Id: I8b298d28dc2bf29112fadd47f379f76c886aa94c

---
+++ vendor_prop.mk
@@ -0,0 +1,11 @@
+#
+# Vendor Properties for Sprint LG V20
+#
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.default_network=10 \
+    ro.cdma.home.operator.numeric=310120 \
+    ro.cdma.home.operator.alpha=Sprint \
+    ro.telephony.default_cdma_sub=1 \
+    telephony.lteOnCdmaDevice=1
